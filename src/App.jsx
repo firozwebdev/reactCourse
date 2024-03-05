1054e4ba719c61ea4589eb6d2e9ac1e79dc067cc
Sabuz <everythingkst@gmail.com>
2024-03-05 20:59:13 +0600

    some file added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Gallery from "./components/Gallery";
+//1.4.P1
+function App() {
+    return (
+        <div>
+            <Gallery />
+            <Gallery /> 
+        </div>
+    );
+}
+
+export default App;
