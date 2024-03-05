36dc7a24be9cf71d2fd3185c067b529cf583a809
Sabuz <everythingkst@gmail.com>
2024-03-05 23:55:57 +0600

    some file added

---
+++ reactCourse/rnext/src/App.jsx
@@ -0,0 +1,12 @@
+import Gallery from "./components/Gallery";
+//1.4
+function App() {
+    return (
+        <div>
+            <Gallery />
+            <Gallery /> 
+        </div>
+    );
+}
+
+export default App;
